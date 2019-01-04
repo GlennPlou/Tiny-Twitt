8abce6b05f3451279dc63a5a4894f990adf77a99
GlennPlou <glenn.plouhinec@etu.univ-nantes.fr>
2019-01-04 22:09:23 +0100

    EnPoint Configuration

--- src/main/java/fr/univnantes/TinyTwittEndPoint.java
+++ src/main/java/fr/univnantes/TinyTwittEndPoint.java
@@ -1,11 +1,14 @@
 package fr.univnantes;
 
+import com.google.api.server.spi.config.Api;
 import fr.univnantes.beans.User;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.util.List;
 
+@Api(name = "TinyTwittAPI")
 public class TinyTwittEndPoint {
+
     public void createUser(String login, String email, String password, String firstname, String lastname) {
 
         List<User> users = ofy().load().type(User.class).filter("login", login).list();
